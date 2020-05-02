32d6bfab6d30ce302654e3d75024c66036377279
abhishekverma02 <abhishek.verma02@nagarro.com>
2020-05-03 01:45:57 +0530

    JSON Server Changes to persist data

--- src/app/core/services/news.service.ts
+++ src/app/core/services/news.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { News } from "../models/news.model";
 import { HttpClient, HttpHeaders, HttpErrorResponse } from "@angular/common/http";
-import { Observable, of, throwError } from "rxjs";
+import { Observable, throwError } from "rxjs";
 import { catchError } from "rxjs/operators"
 
 const httpOptions ={
@@ -13,30 +13,19 @@ const httpOptions ={
 })
 export class NewsService {
 
-    private apiURL = "api/newsList";
-    newsList: News[] = [];
+    private apiJsonUrl = "http://localhost:3010/news";
 
     constructor(private http: HttpClient){}
 
-    addNews(news: News) {
-        try {
-            this.newsList.push(news);
-            this.newsList.sort((val1, val2)=> {return val1.dateAdded > val2.dateAdded ? -1 : 1 })          
-        }
-        catch (error) {
-            console.log(error);
-        }
-    };
-
     fetchNews(): Observable<News[]>{
-        return this.http.get<News[]>(this.apiURL);
+        return this.http.get<News[]>(this.apiJsonUrl);
     }
 
     addNewNews(newNews: News): Observable<News>{
-        return this.http.post<News>(this.apiURL, newNews, httpOptions).pipe(
+        return this.http.post<News>(this.apiJsonUrl, newNews, httpOptions).pipe(
             catchError(this.handelError)
         );
-    }
+    };
 
     private handelError(error: HttpErrorResponse){
         return throwError('their is some error')
